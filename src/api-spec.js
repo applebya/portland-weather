6a75d7b4c8983a9cea5fddedd0e59189aaec0874
Andrew Appleby <andrewappleby22@gmail.com>
2017-08-25 12:36:02 -0700

    Remove api test

--- src/api-spec.js
+++
@@ -1,8 +0,0 @@
-import test from "blue-tape";
-import {fetchFiveDayForecast, fetchCurrentWeather} from "./api";
-
-const cityId = "5746545";
-
-test("Can load 5 day forecast", t => {
-	fetchFiveDayForecast();
-});
